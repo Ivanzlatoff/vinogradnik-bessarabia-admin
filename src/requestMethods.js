a8b74f111560aa75716cf7f85e9cc694f8713861
Ivan Zlatov <ivanzlatoff@gmail.com>
2023-05-27 20:02:22 +0100

    Allowign certain origins CORS

--- src/requestMethods.js
+++ src/requestMethods.js
@@ -7,14 +7,14 @@ export const BASE_URL = 'https://dark-tan-yak-garb.cyclic.app/api';
 export const publicRequest = axios.create({
     baseURL: BASE_URL,
     headers: {
-        "Access-Control-Allow-Origin": "true"
+        "Access-Control-Allow-Origin": ["http://localhost:3001", "https://main.d2znqjapc2ghcc.amplifyapp.com/"]
     }
 });
 
 export const userRequest = axios.create({
     baseURL: BASE_URL,
     headers: {
-        "Access-Control-Allow-Origin": "true"
+        "Access-Control-Allow-Origin": ["http://localhost:3001", "https://main.d2znqjapc2ghcc.amplifyapp.com/"]
     }
 });
 
